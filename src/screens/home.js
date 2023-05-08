20b8e629a56fca20a91bf4b299b20b4ddd832e9a
ViniciusLucchesi <viniciuslucchesi.ti@gmail.com>
2023-05-08 19:25:37 -0300

    Adicionando novos conteúdos a página

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Text, View, Image, TouchableOpacity } from 'react-native'
+import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native'
 
-import {home} from '../styles/estilos.js'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import user from '../../assets/user.png';
+import pancake from '../../assets/pancake.png';
+
+import { home } from '../styles/estilos.js'
 import { useNavigation } from '@react-navigation/native'
 
 
@@ -9,8 +13,39 @@ export default function Welcome() {
     const navigation = useNavigation();
 
     return (
-        <View style={home.background}>
-            <Text>Home</Text>
-        </View>
+        <ScrollView style={home.background}>
+
+            <View>
+                <View style={home.header}>
+                    <TouchableOpacity>
+                        <Ionicons name={"ios-menu-sharp"} size={30} color={"#918AE3"} />
+                    </TouchableOpacity>
+                    <Image source={user} style={home.avatar} />
+                </View>
+                <View style={home.content}>
+                    <View style={home.topContent}>
+                        <Text style={home.title}>
+                            Lanche destaque
+                        </Text>
+                        <View style={home.topCardContent}>
+                            <View>
+                                <Text style={home.subtitle}>
+                                    Descubra
+                                </Text>
+                                <View>
+                                    <Text style={home.topFoodTitle}>
+                                        Melhor comida
+                                    </Text>
+                                    <Text style={home.topFoodTitle}>
+                                        Do dia!
+                                    </Text>
+                                </View>
+                            </View>
+                            <Image source={pancake} style={home.food} />
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
     );
 }
